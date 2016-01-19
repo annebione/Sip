f89a29dc4d80e680fae303fcbd9d1f176854d609
Anne Bione <annebione@github.com>
2016-01-19 05:26:52 -0200

    Sample Sipsource.js

---
+++ sipsource.js
@@ -0,0 +1,29 @@
+const htmlSource = 'path/to/html'
+,	cssSource = 'path/to/css'
+,	jsSource = 'path/to/js'
+,	imgSource = 'path/to/img'
+,	assetsSource = 'path/to/assets';
+
+
+const htmlBuildDest = 'path/to/html'
+,	cssBuildtDest = 'path/to/css'
+,	jsBuildDest = 'path/to/js'
+,	imgBuildDest = 'path/to/img'
+,	assetsBuildDest = 'path/to/assets';
+
+
+
+const misc = [
+	'browser-sync'
+];
+
+
+const css = [
+	'gulp-sass',
+	'base64'
+]
+
+const js = [
+	'jshint',
+	'uglify'
+];
